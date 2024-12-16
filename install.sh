bae9022200d5cb672d3a4bb2edb92da851413ee3
Mas Aji <127058700+mas4ji@users.noreply.github.com>
2024-12-16 19:36:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ pip install -r requirements.txt
 sudo cp ajihunter.py /usr/local/bin/ajihunter
 sudo chmod +x /usr/local/bin/ajihunter
 echo "ajihunter has been installed and is now available as a command."
+
+# Automatically run ajihunter -h
+ajihunter -h
